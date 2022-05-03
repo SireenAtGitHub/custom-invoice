4ac8c47382b6858f1910fa56478acd0c540f2066
kiteretsu-daihyakka <seeyouatthetop42@gmail.com>
2022-05-03 16:21:09 +0530

    added form in base.html and updated validate.js file to handle validation for $ and ^ in item name.
    also changed some of the "return false" statements to "return true" as the api-call.js file was calling invoice/ api even on invalid-form-data.

--- generateInvoice/static/generateInvoice/js/validate.js
+++ generateInvoice/static/generateInvoice/js/validate.js
@@ -10,31 +10,47 @@ function validateForm() {
   name_val = document.getElementById("inputName").value;
   if (name_val == "") {
     alert("Name field should not be empty");
-    return false;
+    return true;
   }
   num_val = document.getElementById("inputNPlate").value;
   if (num_val == "") {
     alert("Number Plate field should not be empty");
-    return false;
+    return true;
   } else {
     if (num_val.length < 10) {
       alert("Enter valid number plate");
-      return false;
+      return true;
     }
   }
 
-  $("input[name='item']").each(function () {
+  var isInvalidItemName = false;
+  $("input[name='item']").each(function() {
     console.log($(this).val());
-    if ($(this).val() == "") {
-      alert("Fill item details");
-      return false;
+    if ($(this).val() == ''){
+        alert("Fill item details");
+        isInvalidItemName = true
+        return false;
+    }
+    else if($(this).val().includes("$") || $(this).val().includes("^")){
+        alert("Please do not enter $ or ^ in Item Name.");
+        isInvalidItemName = true
+        return false;
     }
   });
-  $("input[name='price']").each(function () {
+  if (isInvalidItemName === true){
+    return true
+  }
+
+  var isInvalidItemPrice = false;
+  $("input[name='price']").each(function() {
     console.log($(this).val());
-    if ($(this).val() == "") {
-      alert("Fill price details");
-      return false;
+    if ($(this).val() == ''){
+        alert("Fill price details");
+        isInvalidItemPrice = true
+        return;
     }
   });
+  if (isInvalidItemPrice === true){
+    return true
+  }
 }
