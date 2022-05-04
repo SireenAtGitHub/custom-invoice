90e695b9606f121c2e88da3597d7ad487343bcd2
SireenAtGithub <er.sireen@gmail.com>
2022-05-04 23:15:04 +0530

    Added Count Total feature

--- generateInvoice/static/generateInvoice/js/api-call.js
+++ generateInvoice/static/generateInvoice/js/api-call.js
@@ -1,61 +1,61 @@
 $(document).ready(function () {
-  $('#invoiceForm').submit(function(e) {
-    e.preventDefault();
-    if (validateForm()) {
-      return;
-    }
-    data = {};
-    name = $("[name=item]").serialize();
-    name = decodeURI(name);
-    price = $("[name=price]").serialize();
-    price = decodeURI(price);
-    c_name = $("#inputName").val();
-    plate = $("#inputNPlate").val();
-    p_number = $("#inputPNumber").val();
-    str = generate_str(name, price);
-    data["cname"] = c_name;
-    data["phone"] = p_number;
-    data["number_plate"] = plate;
-    data["items"] = str;
-    data["csrfmiddlewaretoken"] = "{{ csrf_token }}";
-    $.ajax({
-      url: $("#url").val(),
-      type: "POST",
-      data: data,
-      success: function (response) {
-        var binaryData = [];
-        binaryData.push(response);
-        var fileURL = window.URL.createObjectURL(
-          new Blob(binaryData, { type: "application/pdf" })
-        );
-        window.open(fileURL);
-        // to reset the form
-        $('#invoiceForm').trigger("reset");
-        $('.remove_this').each(function(){
-            $(this).trigger('click');
-        })
-      },
+    $("#invoiceForm").submit(function (e) {
+        e.preventDefault();
+        if (validateForm()) {
+            return;
+        }
+        data = {};
+        name = $("[name=item]").serialize();
+        name = decodeURI(name);
+        price = $("[name=price]").serialize();
+        price = decodeURI(price);
+        c_name = $("#inputName").val();
+        plate = $("#inputNPlate").val();
+        p_number = $("#inputPNumber").val();
+        str = generate_str(name, price);
+        data["cname"] = c_name;
+        data["phone"] = p_number;
+        data["number_plate"] = plate;
+        data["items"] = str;
+        data["csrfmiddlewaretoken"] = "{{ csrf_token }}";
+        $.ajax({
+            url: $("#url").val(),
+            type: "POST",
+            data: data,
+            success: function (response) {
+                var binaryData = [];
+                binaryData.push(response);
+                var fileURL = window.URL.createObjectURL(
+                    new Blob(binaryData, { type: "application/pdf" })
+                );
+                window.open(fileURL);
+                // to reset the form
+                $("#invoiceForm").trigger("reset");
+                $(".remove_this").each(function () {
+                    $(this).trigger("click");
+                });
+            },
+        });
     });
-  });
 });
 
 function generate_str(name, price) {
-  name_split = name.split("&");
-  price_split = price.split("&");
-  name_array = [];
-  price_array = [];
-  for (let i = 0; i < name_split.length; i++) {
-    ns_split = name_split[i].split("=");
-    ps_split = price_split[i].split("=");
-    for (let j = 1; j < ns_split.length; j++) {
-      name_array.push(String(ns_split[j]));
-      price_array.push(ps_split[j]);
+    name_split = name.split("&");
+    price_split = price.split("&");
+    name_array = [];
+    price_array = [];
+    for (let i = 0; i < name_split.length; i++) {
+        ns_split = name_split[i].split("=");
+        ps_split = price_split[i].split("=");
+        for (let j = 1; j < ns_split.length; j++) {
+            name_array.push(String(ns_split[j]));
+            price_array.push(ps_split[j]);
+        }
     }
-  }
-  str = "";
-  for (let i = 0; i < price_array.length; i++) {
-    str += name_array[i] + " ^ " + price_array[i];
-    str += " $ ";
-  }
-  return str;
+    str = "";
+    for (let i = 0; i < price_array.length; i++) {
+        str += name_array[i] + " ^ " + price_array[i];
+        str += " $ ";
+    }
+    return str;
 }
--- generateInvoice/static/generateInvoice/js/validate.js
+++ generateInvoice/static/generateInvoice/js/validate.js
@@ -1,56 +1,59 @@
 $(document).ready(function () {
-  $("#inputPNumber").keypress(function () {
-    if ($("#inputPNumber").val().length >= 10) {
-      return false;
-    }
-  });
+    $("#inputPNumber").keypress(function () {
+        if ($("#inputPNumber").val().length >= 10) {
+            return false;
+        }
+    });
 });
 
 function validateForm() {
-  name_val = document.getElementById("inputName").value;
-  if (name_val == "") {
-    alert("Name field should not be empty");
-    return true;
-  }
-  num_val = document.getElementById("inputNPlate").value;
-  if (num_val == "") {
-    alert("Number Plate field should not be empty");
-    return true;
-  } else {
-    if (num_val.length < 10) {
-      alert("Enter valid number plate");
-      return true;
+    name_val = document.getElementById("inputName").value;
+    if (name_val == "") {
+        alert("Name field should not be empty");
+        return true;
     }
-  }
+    num_val = document.getElementById("inputNPlate").value;
+    if (num_val == "") {
+        alert("Number Plate field should not be empty");
+        return true;
+    } else {
+        if (num_val.length < 10) {
+            alert("Enter valid number plate");
+            return true;
+        }
+    }
+    phone_num = $("#inputPNumber").val();
+    if (phone_num.length < 10) {
+        alert("Enter valid phone number");
+        return true;
+    }
+    var isInvalidItemName = false;
+    $("input[name='item']").each(function () {
+        console.log($(this).val());
+        if ($(this).val() == "") {
+            alert("Fill item details");
+            isInvalidItemName = true;
+            return false;
+        } else if ($(this).val().includes("$") || $(this).val().includes("^")) {
+            alert("Please do not enter $ or ^ in Item Name.");
+            isInvalidItemName = true;
+            return false;
+        }
+    });
+    if (isInvalidItemName === true) {
+        return true;
+    }
 
-  var isInvalidItemName = false;
-  $("input[name='item']").each(function() {
-    console.log($(this).val());
-    if ($(this).val() == ''){
-        alert("Fill item details");
-        isInvalidItemName = true
-        return false;
+    var isInvalidItemPrice = false;
+    $("input[name='price']").each(function () {
+        console.log($(this).val());
+        if ($(this).val() == "") {
+            alert("Fill price details");
+            isInvalidItemPrice = true;
+            return;
+        }
+    });
+    if (isInvalidItemPrice === true) {
+        return true;
     }
-    else if($(this).val().includes("$") || $(this).val().includes("^")){
-        alert("Please do not enter $ or ^ in Item Name.");
-        isInvalidItemName = true
-        return false;
-    }
-  });
-  if (isInvalidItemName === true){
-    return true
-  }
-
-  var isInvalidItemPrice = false;
-  $("input[name='price']").each(function() {
-    console.log($(this).val());
-    if ($(this).val() == ''){
-        alert("Fill price details");
-        isInvalidItemPrice = true
-        return;
-    }
-  });
-  if (isInvalidItemPrice === true){
-    return true
-  }
 }
