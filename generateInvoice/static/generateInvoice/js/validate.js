f433764d16a1ea9afa4eb7dc4da607b5c9421fe3
MP589EG <sahil.thakor@ey.com>
2022-05-15 17:29:15 +0530

    1. Added code to remove extra whitespace from each input before doing actual validation. 2. If the validation is correct than assigning that validated and cleaned value (without extra space in beginning, ending or middle) to respective input.

--- generateInvoice/static/generateInvoice/js/validate.js
+++ generateInvoice/static/generateInvoice/js/validate.js
@@ -8,37 +8,48 @@ $(document).ready(function () {
 
 function validateForm() {
     name_val = document.getElementById("inputName").value;
-    if (name_val == "") {
+    let clean_name_val = name_val.trim().replace(/\s+/g," ");
+    if (clean_name_val == "") {
         alert("Name field should not be empty");
         return true;
     }
+    document.getElementById("inputName").value = clean_name_val;
+
     num_val = document.getElementById("inputNPlate").value;
-    if (num_val == "") {
+    let clean_num_val = num_val.trim().replace(/\s+/g,"");
+    if (clean_num_val == "") {
         alert("Number Plate field should not be empty");
         return true;
     } else {
-        if (num_val.length < 10) {
+        if (clean_num_val.length < 10) {
             alert("Enter valid number plate");
             return true;
         }
     }
+    document.getElementById("inputNPlate").value = clean_num_val;
+
     phone_num = $("#inputPNumber").val();
-    if (phone_num.length < 10 && phone_num.length != 0) {
+    let clean_phone_num = phone_num.trim().replace(/\s+/g,"");
+    if (clean_phone_num.length < 10 && clean_phone_num.length != 0) {
         alert("Enter valid phone number");
         return true;
     }
+    $("#inputPNumber").val(clean_phone_num);
+
     var isInvalidItemName = false;
     $("input[name='item']").each(function () {
-        console.log($(this).val());
-        if ($(this).val() == "") {
+        let itemName = $(this).val();
+        let clean_itemName = itemName.trim().replace(/\s+/g," ");
+        if (clean_itemName == "") {
             alert("Fill item details");
             isInvalidItemName = true;
             return false;
-        } else if ($(this).val().includes("$") || $(this).val().includes("^")) {
+        } else if (clean_itemName.includes("$") || clean_itemName.includes("^")) {
             alert("Please do not enter $ or ^ in Item Name.");
             isInvalidItemName = true;
             return false;
         }
+        $(this).val(clean_itemName);
     });
     if (isInvalidItemName === true) {
         return true;
@@ -46,12 +57,14 @@ function validateForm() {
 
     var isInvalidItemPrice = false;
     $("input[name='price']").each(function () {
-        console.log($(this).val());
-        if ($(this).val() == "") {
+        let itemPrice = $(this).val();
+        let clean_itemPrice = itemPrice.trim().replace(/\s+/g,"");
+        if (clean_itemPrice == "") {
             alert("Fill price details");
             isInvalidItemPrice = true;
             return;
         }
+        $(this).val(clean_itemPrice);
     });
     if (isInvalidItemPrice === true) {
         return true;
