f63986f6dac9e3c9f7cbad0e58dc4d0dcdcbfaa2
kiteretsu-daihyakka <seeyouatthetop42@gmail.com>
2023-03-12 15:42:30 +0530

    undoing changes for settings.py which were local environment specific.

--- CustomINVOICE/settings.py
+++ CustomINVOICE/settings.py
@@ -15,13 +15,13 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
